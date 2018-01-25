2c48ab6771f240820b54afdabbbb6e4527983fd5
ColTigh <35714104+ColTigh@users.noreply.github.com>
2018-01-25 15:11:00 -0700

    Add files via upload

---
+++ ipswich.py
@@ -0,0 +1,15 @@
+#my first python script
+#lets check for primes
+
+x = int(input("number please: "))
+ #print (x)
+primebool = True
+for num in range(2, x):
+ if x%num == 0:
+    primebool = False
+if primebool:
+  print ("PRIME!!!!")
+else:
+  print ("not prime")
+# lets see if it works
+#cross our fingers
